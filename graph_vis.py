80eb6d275be3234717dfdeecf06287451cfa03eb
Timerxan <tfalyakhov@gmail.com>
2021-10-06 13:40:43 +0300

    graph_vis updating

--- graph_vis.py
+++ graph_vis.py
@@ -13,14 +13,14 @@ def gr_vis(filt_tags_path, node_cut_off_level=1, edge_cut_off_level=1):
 
     nodes = [node["id"] for node in tags_json["items"]["nodes"]
              if int(node_nc * node["popularity"]) > node_cut_off_level]
-    nodes_size = [2 + int((node_nc * node["popularity"])**0.5)
+    nodes_size = [1 + int((node_nc * node["popularity"])**0.5)
                   for node in tags_json["items"]["nodes"]
                   if int(node_nc * node["popularity"]) > node_cut_off_level]
 
     def color_from_popularity(popularity):  # popularity 0-100
-        if popularity > 75 : return 'red'
-        elif popularity > 50 : return 'yellow'
-        elif popularity > 25 : return 'green'
+        if popularity > 85 : return 'red'
+        elif popularity > 60 : return 'yellow'
+        elif popularity > 35 : return 'green'
         else : return 'blue'
 
     nodes_color = [color_from_popularity(node_nc*node["popularity"])
@@ -33,7 +33,7 @@ def gr_vis(filt_tags_path, node_cut_off_level=1, edge_cut_off_level=1):
              and link["target"] in nodes
              and int(link_nc * link["value"]) > edge_cut_off_level]
 
-    net = Network(800, 800)
+    net = Network(640, 640)
 
     for node, node_size, color in zip(nodes, nodes_size, nodes_color):
         net.add_node(node, hidden=False, shape='dot', color=color, size=node_size, mass=node_size,
@@ -41,44 +41,13 @@ def gr_vis(filt_tags_path, node_cut_off_level=1, edge_cut_off_level=1):
 
     net.add_edges(edges)
     net.inherit_edge_colors(False)
-    net.set_edge_smooth('diagonalCross')
-    net.set_options("""
-                    var options = {
-                      "nodes": {
-                        "font": {
-                          "strokeWidth": 3
-                        }
-                      },
-                      "edges": {
-                        "color": {
-                          "color": "rgba(66,66,65,0.58)",
-                          "highlight": "rgba(255,148,45,1)",
-                          "inherit": false
-                        },
-                        "font": {
-                          "strokeWidth": 42
-                        },
-                        "smooth": {
-                          "type": "cubicBezier",
-                          "forceDirection": "none"
-                        }
-                      },
-                        "physics": {
-                            "barnesHut": {
-                              "gravitationalConstant": -1050,
-                              "centralGravity": 4,
-                              "springLength": 50,
-                              "springConstant": 0.015,
-                              "damping": 0.4,
-                              "avoidOverlap": 0.2
-                            },
-                            "minVelocity": 0.75
-                      }
-                    }
-                    """)
+    with open('var_options.json', 'r', encoding="utf-8") as f:
+        var_options = f.read()
+    net.set_options(f'var_options={var_options}')
+    # net.show_buttons()
     net.write_html('graph_visualisation.html')
-    # net.show('graph_vis.html')
+    # net.show('graph_visualisation.html')
 
 
 if __name__ == '__main__':
-    gr_vis('data/for_visualization/filt-tags_python.json', node_cut_off_level=5, edge_cut_off_level=5)
+    gr_vis('data/for_visualization/filt-tags_machine-learning.json', node_cut_off_level=5, edge_cut_off_level=5)
