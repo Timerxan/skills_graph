f578f010d44eef1aa4773f4745a02ea209e24b80
Timerxan <tfalyakhov@gmail.com>
2021-10-07 04:54:52 +0300

    graph_vis updating

--- flsite.py
+++ flsite.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from graph_vis import gr_vis
+import json
 
 
 app = Flask(__name__)
@@ -9,10 +10,7 @@ app = Flask(__name__)
 def index():
     ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     style = url_for('static', filename='style.css')
-    visualisation = url_for('user_graph', ip=ip)
-    if request.method == 'POST':
-        print(request.form)
-    print(visualisation)
+    visualisation = url_for('user_graph', ip=ip, key_word='python', node_level=5, edge_level=5)
     return render_template('index.html', style=style, vis=visualisation)
 
 
@@ -20,20 +18,22 @@ def index():
 def send():
     ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     style = url_for('static', filename='style.css')
-    visualisation = url_for('user_graph', ip=ip)
     if request.method == 'POST':
-        print(request.form)
-    print(visualisation)
-    return render_template('index.html', style=style, vis=visualisation)
+        key_word = request.form['key_word']
+        key_word, node_level, edge_level = key_word.split(':')
+        visualisation = url_for('user_graph', ip=ip, key_word=key_word,
+                                node_level=int(node_level), edge_level=int(edge_level))
+        return render_template('index.html', style=style, vis=visualisation)
 
 
-@app.route('/user_graph<ip>', methods=['GET'])
-def user_graph(ip):
-    path = gr_vis(ip)
+@app.route('/user_graph_<ip>_<key_word>_<node_level>_<edge_level>', methods=['POST', 'GET'])
+def user_graph(ip, key_word, node_level, edge_level):
+    path = gr_vis(ip, key_word, int(node_level), int(edge_level))
     with open(path, 'r', encoding="utf-8") as f:
         graph_html = f.read()
+
     return graph_html
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- graph_vis.py
+++ graph_vis.py
@@ -2,9 +2,12 @@ from pyvis.network import Network
 import json
 
 
-def gr_vis(ip='None', filt_tags_path='data/for_visualization/filt-tags_python.json',
-           node_cut_off_level=1, edge_cut_off_level=1):
-    with open(filt_tags_path, 'r', encoding="utf-8") as f:
+def gr_vis(ip='None', key_word='python', node_level=5, edge_level=5):
+
+    with open('data/for_visualization/index.json', 'r', encoding="utf-8") as f:
+        key_words_json = json.load(f)
+
+    with open(f"data/for_visualization/{key_words_json[key_word]}", 'r', encoding="utf-8") as f:
         tags_json = json.load(f)
 
     node_max_popularity = max([node["popularity"] for node in tags_json["items"]["nodes"]])
@@ -13,10 +16,10 @@ def gr_vis(ip='None', filt_tags_path='data/for_visualization/filt-tags_python.js
     link_nc = 100/link_max_value  # link normalization coefficient
 
     nodes = [node["id"] for node in tags_json["items"]["nodes"]
-             if int(node_nc * node["popularity"]) > node_cut_off_level]
+             if int(node_nc * node["popularity"]) > node_level]
     nodes_size = [1 + int((node_nc * node["popularity"])**0.5)
                   for node in tags_json["items"]["nodes"]
-                  if int(node_nc * node["popularity"]) > node_cut_off_level]
+                  if int(node_nc * node["popularity"]) > node_level]
 
     def color_from_popularity(popularity):  # popularity 0-100
         if popularity > 85 : return 'red'
@@ -26,13 +29,13 @@ def gr_vis(ip='None', filt_tags_path='data/for_visualization/filt-tags_python.js
 
     nodes_color = [color_from_popularity(node_nc*node["popularity"])
                    for node in tags_json["items"]["nodes"]
-                   if int(node_nc * node["popularity"]) > node_cut_off_level]
+                   if int(node_nc * node["popularity"]) > node_level]
 
     edges = [(link["source"], link["target"], int(link_nc * link["value"]/15))
              for link in tags_json["items"]["links"]
              if link["source"] in nodes
              and link["target"] in nodes
-             and int(link_nc * link["value"]) > edge_cut_off_level]
+             and int(link_nc * link["value"]) > edge_level]
 
     net = Network(800, 800)
 
@@ -46,10 +49,10 @@ def gr_vis(ip='None', filt_tags_path='data/for_visualization/filt-tags_python.js
         var_options = f.read()
     net.set_options(f'{var_options}')
     # net.show_buttons()
-    path = f'tmp/graph_visualisation{ip}.html'
+    path = f'tmp/graph_visualisation{ip}_{key_word}.html'
     net.save_graph(path)
     return path
 
 
 if __name__ == '__main__':
-    gr_vis('123234', 'data/for_visualization/filt-tags_python.json', node_cut_off_level=5, edge_cut_off_level=5)
+    gr_vis('123234', key_word='python', node_level=5, edge_level=5)
