bc662c1b4a7fa93b9f1c2081a98eb8d18f5e6ff8
Timerxan <tfalyakhov@gmail.com>
2021-10-15 18:11:16 +0300

    graph_vis updating

--- scrapers/sgl_db_operation.py
+++ scrapers/sgl_db_operation.py
@@ -1,30 +1,49 @@
 import sqlite3 as sq
 
-
-def execute_query(query):
-    with sq.connect("scrapers/sql_database/hh_vacancies.db") as con:
-        cursor = con.cursor()
-        cursor.execute(query)
+DATA_BASE = "scrapers/sql_database/hh_vacancies.db"
 
 
-query_create_table = """
-CREATE TABLE IF NOT EXISTS skills_by_day 
-(
-day INTEGER
-)
-"""
-query_add_column = """
-ALTER TABLE skills_by_day ADD COLUMN skill2
-"""
-query_insert_into_table = """
-INSERT INTO skills_by_day (day,skill)
-VALUES (15,3)
-"""
-query_update_table = """
-UPDATE skills_by_day
-SET skill = skill +1
-"""
+def execute_query(db_name, query):
+    try:
+        with sq.connect(db_name) as con:
+            cursor = con.cursor()
+            cursor.execute(query)
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
 
+
+def query_generator(query: str, options: list):
+    for a in options:
+        query = query.replace('|?|', a, 1)
+    return query
+
+
+def create_table(table_name: str, columns: list, db_name=DATA_BASE):
+    pattern = """ CREATE TABLE IF NOT EXISTS |?| ( |?| ) """
+    query = query_generator(pattern, [table_name, ','.join(columns)])
+    return execute_query(db_name, query)
+
+
+def add_column(table_name: str, column: str, db_name=DATA_BASE):
+    pattern = """ ALTER TABLE |?| ADD COLUMN |?| """
+    query = query_generator(pattern, [table_name, column])
+    return execute_query(db_name, query)
+
+
+def insert_into_table(table_name: str, columns: list, values: list, db_name=DATA_BASE):
+    pattern = """ INSERT INTO |?| ( |?| ) VALUES ( |?| ) """
+    query = query_generator(pattern, [table_name, ','.join(columns), ','.join(values)])
+    return execute_query(db_name, query)
+
+
+def update_table(table_name: str, columns: list, values: list, conditions: str, db_name=DATA_BASE):
+    pattern = """ UPDATE |?| SET |?| WHERE |?| """
+    query = query_generator(pattern,
+                            [table_name, ','.join([f'{col}={val}' for col, val in zip(columns, values)]), conditions])
+    return execute_query(db_name, query)
 
 if __name__ == '__main__':
-    execute_query(query_add_column)
+    update_table('skills_by_day', ['skill_1', 'skill_2'], ['5', '2'], 'day = 5')
+
